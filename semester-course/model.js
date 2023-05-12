ed2bf9f300dbc7e0597af10998861f2b73870494
arman <armanheids@gmail.com>
2023-05-13 01:17:20 +0330

    semester added

---
+++ semester-course/model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+const acceptedCourseModel = {
+    classDate: Date,
+    examDate: Date,
+    capacity: Number,
+    professor: 
+}
+
+module.exports = acceptedCourseModel
