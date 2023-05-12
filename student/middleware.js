580bc59d898d7df98d290f60bf726259c3fe44f5
arman <armanheids@gmail.com>
2023-05-13 01:17:37 +0330

    semester added

--- student/middleware.js
+++ student/middleware.js
@@ -1,16 +1,39 @@
 const validator = require('express-validator');
 const studentValidator = require('./validator.js')
 
-module.exports.createMiddleware = () => {
+module.exports.createMiddleware = services => {
     const middleware = {}
 
+    middleware.isStudent = async (req, res, next) => {
+        const filter = { username: req.user.username }
+        const user = await services.userService.getOneUser(filter)
+
+        if (!user) {
+            return res.status(400).send('Username does not exist.')
+        }
+
+        const userType = require('../types/user.js')
+        if (user.userType != userType.student.description) {
+            return res.sendStatus(403)
+        }
+
+        req.student = user
+        next()
+    }
+
     middleware.updateStudent = async (req, res, next) => {
-        studentValidator.updateStudent.run(req)
+        await studentValidator.updateStudent.run(req)
         const validationResult = validator.validationResult(req)
         if (!validationResult.isEmpty()) {
             return res.status(400).send(validationResult.array())
         }
 
+        const validData = validator.matchedData(req)
+        if (req.student.username != validData.id) {
+            return res.sendStatus(403)
+        }
+
+        req.validData = validData
         next()
     }
 
