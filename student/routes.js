c680abdba4dd80f91b70970adfda6a542b591a8f
arman <armanheids@gmail.com>
2023-05-13 01:17:55 +0330

    student added

--- student/routes.js
+++ student/routes.js
@@ -6,7 +6,7 @@ module.exports.setup = (app, middleware, handler) => {
     const authenticateToken = require('../jwt').authenticateToken
     // router.use(authenticateToken)
 
-    router.put('/student/:id', middleware.updateStudent, handler.updateStudent)
-    router.get('/courses', middleware.findAllCourses, handler.findAllCourses)
-    router.get('/courses/:id', middleware.findOneCourse, handler.findOneCourse)
+    router.put('/student/:id', authenticateToken, middleware.isStudent, middleware.updateStudent, handler.updateStudent)
+    router.get('/courses', authenticateToken, middleware.isStudent, middleware.findAllCourses, handler.findAllCourses)
+    router.get('/courses/:id', authenticateToken, middleware.isStudent, middleware.findOneCourse, handler.findOneCourse)
 }
