c7538c421f31a207346d80435e0a0fa0feb26619
arman <armanheids@gmail.com>
2023-05-13 01:17:28 +0330

    semester added

--- student/handler.js
+++ student/handler.js
@@ -1,16 +1,33 @@
-const validator = require('express-validator')
+const response = require('./response')
 
 module.exports.createHandler = services => {
     const handler = {}
 
-    handler.updateStudent = (req, res) => {
-        const data = validator.matchedData(req)
-        console.log('hi there ${data.username}')
+    handler.updateStudent = async (req, res) => {
+        const data = req.validData
+        const student = req.student
+
+        if (student.username != data.username) {
+            const filter = { username: data.username }
+            const student = await services.studentService.getOneStudent(filter)
+
+            if (student) {
+                return res.status(400).send('This username is already registered.')
+            }
+        }
+
+        delete data.idParam
+        const filter = { username: student.username }
+        const updatedStudent = await services.studentService.updateStudent(filter, data)
+
+        if (!updatedStudent) {
+            return res.status(400).send('ID does not exist.')
+        }
+
+        res.send(response.updateStudent(updatedStudent))
     }
 
     handler.findAllCourses = (req, res) => {
-        console.log(req.user)
-        res.send('hi')
     }
 
     handler.findOneCourse = (req, res) => {
